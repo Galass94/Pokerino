9ebc7f76a157c55943b4070fcd88ea8313de759b
Galass94 <109736913+Galass94@users.noreply.github.com>
2023-01-11 13:10:40 +0100

    Update Pokerino.py

--- Pokerino.py
+++ Pokerino.py
@@ -4,7 +4,7 @@ import os
 suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
 cardDisplay = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
 cardValues = [2,3,4,5,6,7,8,9,10,11,12,13,14]
-playerClasses = ["w", "m", "t", "d", "r"]
+playerClasses = ["w", "m", "t", "d","b", "r"]
 
 class Card:
     def __init__(self, value, suit, display):
@@ -26,7 +26,10 @@ class Card:
 
     def __str__(self):
         return f"{cardDisplay[cardValues.index(self.value)]:>2} of {self.suit}"
-        
+    def __eq__(self, other):
+        if isinstance(other, Card):
+            return self.value == other.value and self.suit == other.suit
+        return False
 class Deck:
     deck = []
     def __init__(self):
@@ -35,7 +38,6 @@ class Deck:
                 self.deck.append(Card(value, suit, cardDisplay[cardValues.index(value)]))
     def shuffle(self):
         random.shuffle(self.deck)
-
 class Table:
     def __init__(self):
         self.listTableCards = []
@@ -66,9 +68,6 @@ class Table:
                 nextTurn(False)
             else:
                 nextTurn(True)
-        # os.system("pause")
-
-
 class Player:
     def __init__(self):
         self.name = input("Enter Player Name: ")
@@ -76,8 +75,8 @@ class Player:
             try:
                 print(f"What class are you, {self.getName()}?\n(W)arrior - Can destroy on card on the table\n(M)age - Can change the values of up to 2 cards by 1")
                 print("(T)hief - Can trade one card from their hand with one card on the table\n(D)eprived - BONK")
-                print("(R)andom - Get a random ability")
-                self.ability = input()
+                print("(R)andom - Get a random ability\n(B)ard - Gets 1 Mulligan")
+                self.ability = input().lower()
                 if self.ability not in playerClasses: raise ValueError
                 break
             except ValueError as e:
@@ -86,17 +85,17 @@ class Player:
                 os.system("cls")
         playerList.append(self)
         self.handsize=2
-        self.money = 100
+        self.money=100
         self.handCards = []
+        self.highestCard: str
         self.pairs = []
         self.triples = []
         self.quads = []
         self.pentas = []
         self.hexas = []
-        # self.straight = [] DONE
-        self.flush = ""
-        self.straightFlush = [[],[]]
-        self.royalFlush = [[],[]]
+        self.flush: str
+        self.straightFlush = []
+        self.royalFlush = []
         self.score=0
         if self.ability == "w":
             self.playerClass = "Warrior"
@@ -110,6 +109,9 @@ class Player:
         elif self.ability == "d":
             self.playerClass = "Deprived"
             self.abilityCount = 3
+        elif self.ability == "b":
+            self.playerClass = "Bard"
+            self.abilityCount = 1
         elif self.ability == "r":
             self.playerClass = "Random"
             self.abilityCount = 2
@@ -132,7 +134,6 @@ class Player:
             except ValueError as e:
                 print("Enter a valid number!")
         table.listTableCards.pop(choice2-1)
-
     def mageAbility(self):
         print("You activated your ability to change the value of one card on the table!")
         while True:
@@ -153,7 +154,6 @@ class Player:
                 table.listTableCards[choice2-1].setValue(14)
             else:
                 table.listTableCards[choice2-1].setValue(cardValues[cardValues.index(table.listTableCards[choice2-1].value)-1])
-    
     def thiefAbility(self):
         print("You activated your ability to switch one card from the table with one from your hand!")
         while True:
@@ -172,14 +172,30 @@ class Player:
                 print("Enter a valid number!")
         self.handCards.append(table.listTableCards.pop(choiceT-1))
         table.listTableCards.append(self.handCards.pop(choiceT2-1))
-
     def deprivedAbility(self):
         temp = random.randint(0, 99)
         if temp <= 4:
-            print("BONK!!!")
+            print(f"{playerList[random.randint(0,len(playerList)-1)].getName()} got BONKed!!!")
         else :
             print("BONK!")
-
+    def bardAbility(self):
+        print(f"{self.getName()}, do you want to draw a new hand? (Y/N)")
+        choiceB = input().lower()
+        match(choiceB):
+            case "y":
+                for i in range(2):
+                    # print(f"{self.handCards[0]} was put into the deck.")
+                    deck.deck.append(self.handCards.pop(0))
+                deck.shuffle()
+                print()
+                for i in range(0,2):
+                    # print(f"{self.getName()} drew a {deck.deck[0]}")
+                    self.handCards.append(deck.deck.pop(0))
+                self.abilityCount -= 1
+            case "n":
+                self.abilityCount -= 1
+                pass
+        pass
     def randomAbility(self):
         temp = random.randint(0, len(playerClasses)-2)
         match temp:
@@ -195,8 +211,10 @@ class Player:
             case 3: 
                 print("DEPRIVED ABILITY") 
                 self.deprivedAbility()
+            case 4:
+                print("BARD ABILITY")
+                self.bardAbility()
         pass
-
     def activateAbility(self):
         while self.abilityCount > 0:
             choice = input(f"\n{self.getName()}, do you want to activate an ability? - ")
@@ -220,43 +238,60 @@ class Player:
                         return True
                 case 'n':
                     return False
-
+    def getAbility(self):
+        return self.ability
     def addScore(self, x):
         self.score += x
-
     def getScore(self):
-        print(f"{self.getName()}: {self.score} points\n")
-
+        return self.score
+    def printScore(self):
+        print(f"{self.getName()}: {self.score} points")
     def printHand(self):
         print(f"--- {self.getName()}'s Hand ({self.playerClass})---")
         for card in self.handCards:
             print(card)
         print()
-    
     def addMoney(self, x):
         if x > self.money:
             self.money = 0
         else: self.money += x
+    def setHandValues(self):
+        self.handValues = []
+        for card in self.handCards:
+            self.handValues.append(card.getValue())
+    def getHandValues(self):
+        return self.handValues
     def setgetHandValues(self):
         self.handValues = []
         for card in self.handCards:
             self.handValues.append(card.getValue())
-        return self.handValues
-
+            return self.handValues
+    def getHandCards(self):
+        return self.handCards
+    def setHighestCard(self, card):
+        self.highestCard = card
+    def getHighestCard(self):
+        return self.highestCard
     def setStraight(self, cards):
         self.straight = cards
     def getStraight(self):
         return self.straight
-    def setRoyalFlush(self, suit, cards):
-        self.royalFlush = [suit, cards]
+    def setRoyalFlush(self, card):
+        self.royalFlush.append(card)
     def getRoyalFlush(self):
         return self.royalFlush
     def setStraightFlush(self, suit, cards):
         self.straightFlush = [suit, cards]
     def getStraightFlush(self):
         return self.straightFlush
-
-
+    def emptyRoyalFlush(self):
+        self.royalFlush.clear()
+    def setFlush(self, suit):
+        self.flush = suit
+    def getFlush(self):
+        return self.flush
+    def setPairs(self, value):
+        self.pairs.append(value)
 def nextTurn(draw):
     print("\n\n")
     os.system("pause")
@@ -273,17 +308,28 @@ def nextTurn(draw):
         else:
             nextTurn(True)
     else: pass
-
+def checkRoyalFlush(player):
+    royalFlush = [[Card(14, "Diamonds", "A"), Card(13, "Diamonds", "K"), Card(12, "Diamonds", "Q"), Card(11, "Diamonds", "J"), Card(10, "Diamonds", 10)],
+    [Card(14, "Hearts", "A"), Card(13, "Hearts", "K"), Card(12, "Hearts", "Q"), Card(11, "Hearts", "J"), Card(10, "Hearts", 10)],
+    [Card(14, "Clubs", "A"), Card(13, "Clubs", "K"), Card(12, "Clubs", "Q"), Card(11, "Clubs", "J"), Card(10, "Clubs", 10)],
+    [Card(14, "Spades", "A"), Card(13, "Spades", "K"), Card(12, "Spades", "Q"), Card(11, "Spades", "J"), Card(10, "Spades", 10)]]
+    for i in range(4):
+        count=0
+        player.emptyRoyalFlush()
+        for j in range(5):
+            for card in player.handCards:
+                if card == royalFlush[i][j]:
+                    # print(f"{card}: I'm in there")
+                    count += 1
+                    player.setRoyalFlush(royalFlush[i][j])
+                    break
+        if count==5:
+            break
+    if count==5:
+        return True
+    else:
+        return False
 def checkStraight(player):
-    def isFlush():
-        flushCount = 0
-        for i in range(0, len(player.getStraight())-1):
-            if player.getStraight()[i].getSuit() == player.getStraight()[i+1].getSuit():
-                flushCount += 1
-            else:
-                flushCount = 0
-        return flushCount
-
     count=0
     straightCards = []
     hasStraight=False
@@ -291,10 +337,6 @@ def checkStraight(player):
     for i in range(0,len(values)-1):
         if player.handCards[i].getValue() == player.handCards[i+1].getValue():
             continue
-    # for value in values:
-    #     if value-1 in values:
-    #         count += 1
-            
         elif (player.handCards[i].getValue())-1 == player.handCards[i+1].getValue():
             count += 1
             straightCards.append(player.handCards[i])
@@ -307,21 +349,15 @@ def checkStraight(player):
             hasStraight = True
             break
     if hasStraight:
+        if checkRoyalFlush(player):
+            return "Royal Flush"
         player.setStraight(straightCards)
-        if player.getStraight()[0].getValue() == 14:
-            if isFlush() == 4:
-                player.setRoyalFlush(straightCards[0].getSuit(),straightCards)
-                return "Royal Flush"
-            else:
-                return "Royal Straight"
+        if checkFlush(player):
+            player.setStraightFlush(straightCards[0].getSuit(), straightCards)
+            return "Straight Flush"
         else:
-            if isFlush() == 4:
-                player.setStraightFlush(straightCards[0].getSuit(), straightCards)
-                return "Straight Flush"
-            else:
-                return "Straight"
-    
-def checkFlush(player):
+            return "Straight"
+def checkFlush(player):     # Check if player has Flush and if they do, return the suit of the flush
     diamondCount = 0
     heartCount = 0
     spadeCount = 0
@@ -337,20 +373,23 @@ def checkFlush(player):
         if card.getSuit() == "Clubs":
             clubCount += 1
     if diamondCount >= 5:
+        player.setFlush("Diamonds")
         # print(f"{player.getName()} has a Flush of Diamonds!")
         hasFlush = True
     elif heartCount >= 5:
+        player.setFlush("Hearts")
         # print(f"{player.getName()} has a Flush of Hearts!")
         hasFlush = True
     elif spadeCount >= 5:
+        player.setFlush("Spades")
         # print(f"{player.getName()} has a Flush of Spades!")
         hasFlush = True
     elif clubCount >= 5:
+        player.setFlush("Clubs")
         # print(f"{player.getName()} has a Flush of Clubs!")
         hasFlush = True
     return hasFlush
-
-def checkMultiples(player):
+def checkMultiples(player): # Check if player has multiples of a value and if they do, return the highest multiple
     for card in player.handCards:
         card.winners["Pair"] = False
         card.winners["Triple"] = False
@@ -438,26 +477,23 @@ def checkMultiples(player):
                 print(f"{player.getName()} has a pair of {cardDisplay[cardValues.index(card)]}s")
                 player.addScore(2)
     return hasMultiples
-
-def highestCard(player):
+def highestCard(player):    # If player has nothing else, output the highest card in their Hand
     player.addScore(1)
     print(f"{player.getName()}'s highest card is {cardDisplay[cardValues.index(player.handCards[0].getValue())]}")
-
-def values(player): #FIXME: Royal Flush not correct
+    player.setHighestCard(player.getHandCards()[0])
+def values(player):
     #Karten vom Tisch zu Händen hinzufügen
     for card in table.listTableCards:
         player.handCards.append(card)
-    def getCardValue(card):
-        return card.getValue()
-    def getCardSuit(card):
-        return card.getSuit()
-    player.handCards.sort(key=getCardSuit)
-    player.handCards.sort(key=getCardValue,reverse=True)
+    player.handCards.sort(key=Card.getValue,reverse=True)
     while True:
         if checkStraight(player) == "Royal Flush":
             player.addScore(40)
-            print(f"{player.getName()} has a ROYAL FLUSH!!!") #FIXME: Royal Flush with different 
-            for card in player.getRoyalFlush()[1]:
+            if player.getAbility() != "d":
+                print(f"{player.getName()} has a ROYAL FLUSH!!!")
+            else:
+                print(f"{player.getName()}: Such Royal Flush, Much Wow, Great!")
+            for card in player.getRoyalFlush():
                 print(card)
             break
         elif checkStraight(player) == "Royal Straight":
@@ -468,7 +504,10 @@ def values(player): #FIXME: Royal Flush not correct
             break
         elif checkStraight(player) == "Straight Flush":
             player.addScore(20)
-            print(f"{player.getName()} has a Straight Flush!")
+            print(f"{player.getName()} has a Straight Flush of", end=" ")
+            print(player.getStraightFlush()[0])
+            for card in player.getStraightFlush()[1]:
+                print(f"{card.getValue():>2} of {player.getStraightFlush()[0]}")
             break
         elif checkStraight(player) == "Straight":
             player.addScore(10)
@@ -476,24 +515,44 @@ def values(player): #FIXME: Royal Flush not correct
             break
         elif checkFlush(player):
             player.addScore(12)
-            print(f"{player.getName()} has a Flush!")
+            print(f"{player.getName()} has a Flush of", end=" ")
+            print(player.getFlush())
             break
         elif checkMultiples(player):
             break
         else: 
             highestCard(player)
             break
-
 def stackDeck():
-    deck.deck[0] = Card(14,"Diamonds", 'A')
-    deck.deck[1] = Card(13,"Diamonds", 'K')
-    deck.deck[2] = Card(12,"Diamonds", 'Q')
-    deck.deck[3] = Card(11,"Diamonds", 'J')
-    deck.deck[4] = Card(10,"Diamonds", 10)
+    deck.deck[0] = Card(2,"Diamonds", 2)
+    deck.deck[1] = Card(3,"Diamonds", 3)
+    deck.deck[2] = Card(4,"Diamonds", 4)
+    deck.deck[3] = Card(5,"Diamonds", 5)
+    deck.deck[4] = Card(6,"Diamonds", 6)
+    deck.deck[5] = Card(4, "Spades", 4)
+def compareCards(players):
+    if len(players)>=1:
+        playerWin = players[0]
+        for i in range(len(players)-1):
+            if playerWin == playerList[i]:
+                continue
+            for j in range(7):
+                # print("\nDEBUG : "+playerWin.getName()+": "+str(playerWin.getHandCards()[j]))
+                # print("DEBUG : "+players[i].getName()+": "+str(players[i].getHandCards()[j]))
+                if playerWin.getHandCards()[j].getValue() > players[i].getHandCards()[j].getValue():
+                    # print("DEBUG : PlayerWin stays")
+                    break
+                elif playerWin.getHandCards()[j].getValue() == players[i].getHandCards()[j].getValue():
+                    pass
+                else:
+                    playerWin = playerList[i]
+                    # print("DEBUG : PlayerWin changes to "+players[i].getName())
+                    break
+        return playerWin
 
 deck = Deck()
-deck.shuffle()
-# deck.shuffle()
+for i in range(7):
+    deck.shuffle()
 # stackDeck()
 os.system("cls")
 playerList = []
@@ -509,6 +568,9 @@ for i in range(playerCount):
 for player in playerList:
     player.printHand()
 #TODO: Implement betting system
+for player in playerList:
+    if player.getAbility().lower() == "b":
+        player.bardAbility()
 os.system("pause")
 os.system("cls")
 table = Table()
@@ -516,5 +578,31 @@ table.turnOne()
 print("\n")
 for player in playerList:
     values(player)
-    player.getScore()
+    player.printScore()
+    # print("Highest card: " + str(player.getHandCards()[0])+"\n")
+#Check who has highest card
+#Compare highest card between 2 players, if they're the same, compare the next cards
+#If one card is higher than the other, compare the winning player with the next player
+playerList.sort(key=Player.getScore, reverse=True)
+for player in playerList:
+    print(player.getName())
+playersToCompare = []
+playerWin = playerList[0]
+for i in range(playerCount-1):
+    if playerList[i].getScore() == playerList[i+1].getScore():
+        if playerList[i] not in playersToCompare:
+            playersToCompare.append(playerList[i])
+        else: continue
+if len(playersToCompare)>=2:
+    playerWin = compareCards(playersToCompare)
+else:
+    playerWin = playerList[0]
+    for j in range(0, playerCount):
+        if playerWin.getScore() > playerList[j].getScore():
+            playerWin = playerList[j]
+        elif playerWin.getScore() == playerList[j].getScore():
+            pass
+        else:
+            playerWin = playerList[j]
+print("Winner is: "+ playerWin.getName())
 os.system("pause")
